2ccd6007285e2f7cc160f5db0b0cdd4e5c0336ae
jjhernandezm01 <jjhernandezm01@aula1-02>
2015-10-13 19:56:11 +0200

    Cambios en la prueba forzada

---
+++ Entornos EjP1/src/entornos/ejp1/EntornosEjP1.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entornos.ejp1;
+
+/**
+ *
+ * @author jjhernandezm01
+ */
+public class EntornosEjP1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        int num1=10, num2=5, resultado=0;
+        
+        resultado = num1 * num2;
+        
+        System.out.println("La Multiplicacion de " +num1+ " * " + num2 + " = " +resultado);
+        
+        
+        
+    }
+    
+}
