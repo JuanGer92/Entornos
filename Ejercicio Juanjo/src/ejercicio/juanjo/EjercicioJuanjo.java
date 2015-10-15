17178b63c2ce9b8c0266be5a02f5b8b0e6033887
jjhernandezm01 <jjhernandezm01@aula1-02>
2015-10-15 17:44:48 +0200

    Cambios en la prueba forzada

---
+++ Ejercicio Juanjo/src/ejercicio/juanjo/EjercicioJuanjo.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio.juanjo;
+
+/**
+ *
+ * @author jjhernandezm01
+ */
+public class EjercicioJuanjo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int num1 = 10, num2 = 15, resultado;
+        
+        resultado = num1 * num2;
+        
+        System.out.println(resultado);
+    }
+    
+}
